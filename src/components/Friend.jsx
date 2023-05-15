a098d1192992d0d1ce6a440f669c8dece95dc17d
Artur Vanivskyi <vanivskyi.artur@gmail.com>
2023-05-15 16:35:50 -0400

    aaded states for friend component as well as all imports

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setFriends } from "state";
+import FlexBetween from "./FlexBetween";
+import UserImage from "./UserImage";
 
 const Friend = () => {
-  return (
-    <div>Friend</div>
-  )
-}
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { _id } = useSelector((state) => state.user);
+  const token = useSelector((state) => state.token);
+  const friends = useSelector((state) => state.user.friends);
 
-export default Friend
+  const { palette } = useTheme();
+  const primaryLight = palette.primary.light;
+  const primaryDark = palette.primary.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  return <div>Friend</div>;
+};
+
+export default Friend;
