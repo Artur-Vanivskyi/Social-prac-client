a34e8018dd0a40ee735cfdaecbf93ce4dafe154d
Artur Vanivskyi <vanivskyi.artur@gmail.com>
2023-05-14 21:17:52 -0400

    userwidget is complete

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -115,19 +115,28 @@ const UserWidget = ({ userId, picturePath }) => {
           </Typography>
           <FlexBetween gap="1rem" mb="0.5rem">
             <FlexBetween gap="1rem">
-                <img src="../assets/twitter.png" alt="twitter" />
-                <Box>
-                    <Typography color={main} fontWeight="500">
-                        Twitter
-
-                    </Typography>
-                    <Typography color={medium}>
-                        Social Network
-                    </Typography>
-                </Box>
-
+              <img src="../assets/twitter.png" alt="twitter" />
+              <Box>
+                <Typography color={main} fontWeight="500">
+                  Twitter
+                </Typography>
+                <Typography color={medium}>Social Network</Typography>
+              </Box>
             </FlexBetween>
+            <EditOutlined sx={{ color: main }} />
+          </FlexBetween>
 
+          <FlexBetween gap="1rem">
+            <FlexBetween gap="1rem">
+              <img src="../assets/linkedin.png" alt="linkedin" />
+              <Box>
+                <Typography color={main} fontWeight="500">
+                  Linkedin
+                </Typography>
+                <Typography color={medium}>Network Platform</Typography>
+              </Box>
+            </FlexBetween>
+            <EditOutlined sx={{ color: main }} />
           </FlexBetween>
         </Box>
       </FlexBetween>
