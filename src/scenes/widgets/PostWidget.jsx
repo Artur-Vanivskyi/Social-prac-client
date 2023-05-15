c5b42f9219d4b333bd9d925a31669c32b7639ca6
Artur Vanivskyi <vanivskyi.artur@gmail.com>
2023-05-15 16:33:53 -0400

    added updated post functionality

--- src/scenes/widgets/PostWidget.jsx
+++ src/scenes/widgets/PostWidget.jsx
@@ -24,7 +24,6 @@ const PostWidget = ({
   likes,
   comments,
 }) => {
-  
   const [isComments, setIsComments] = useState(false);
   const dispatch = useDispatch();
   const token = useSelector((state) => state.token);
@@ -36,6 +35,23 @@ const PostWidget = ({
   const main = palette.neutral.main;
   const primary = palette.primary.main;
 
+  const patchLike = async () => {
+    const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userId: loggedInUserId }),
+    });
+
+    const updatedPost = await response.json();
+    dispatch(setPost({ post: updatedPost }));
+  };
+
+  return (
+    
+  )
 
 
 };
